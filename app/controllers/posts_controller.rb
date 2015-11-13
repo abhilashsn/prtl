ce20317662a7a17cc65a90b45e6a00184e15fc22
abhilash <abhilashsn83@gmail.com>
2015-11-13 18:31:34 +0530

    add

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,35 +1,51 @@
 class PostsController < ApplicationController
-	  before_action :set_posts, only: [:show, :edit, :update, :destroy]
+	before_action :find_post, only: [:show, :edit, :update, :destroy]
+	before_action :authenticate_user!
 
-	def index 
-		@posts = Post.all
+	def index
+		@posts = Post.all.order("created_at DESC")
 	end
 
-	def new 
-		@posts = Post.new
+	def show
 	end
+
+	def new
+		@post = current_user.posts.build
+	end
+
 	def create
-		@posts = Post.new(post_params)
-		if @posts.save
-			redirect_to posts_path
-		else 
-			render :action => "new"
-	end
-	end
-	
-	def show 
+		@post = current_user.posts.build(post_params)
+
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
 	end
 
+	def edit
+	end
 
-private 
+	def update
+		if @post.update(post_params)
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
 
-	def set_posts
+	def destroy
+		@post.destroy
+		redirect_to root_path
+	end
+
+	private
+
+	def find_post
 		@post = Post.find(params[:id])
 	end
 
-
 	def post_params
-		params[:post].permit(:title,:description)
+		params.require(:post).permit(:title, :description)
 	end
-
 end
